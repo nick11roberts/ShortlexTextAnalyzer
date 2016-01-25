b0341f2e598b4aaeecdab4639ed7cbb736aacd8e
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2016-01-25 11:33:21 -0800

    Added documentation to my tests.

--- src/io/github/nick11roberts/ShortlexTextAnalyzer/AnalyzerTest.java
+++ src/io/github/nick11roberts/ShortlexTextAnalyzer/AnalyzerTest.java
@@ -1,16 +1,25 @@
+/**
+ * 
+ * @author Nicholas Roberts
+ *
+ */
 package io.github.nick11roberts.ShortlexTextAnalyzer;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * A test class for the Analyzer class. 
+ */
 public class AnalyzerTest {
 	
+	// Message 
 	private String generateReportTestMessage = "Test case: ";
 	
 	private Analyzer analyzer = new Analyzer();
-	private String[] testCases = {};
-	private String[] testCasesOut = {};
+	private String[] testCases = {"", "the"};
+	private String[] testCasesOut = {"the SD", "the"};
 
 	@Test
 	public void testGenerateReport() {
